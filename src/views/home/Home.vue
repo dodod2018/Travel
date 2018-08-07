2bb57336796b74828c3263af1b528120f151b7d2
jiangyounan <neverxuqi@163.com>
2018-08-07 15:42:06 +0800

    swiper

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,12 +1,14 @@
 <template>
     <div>
         <home-header/>
+        <home-swiper/>
     </div>
 </template>
 <script>
 import HomeHeader from './components/Header'
+import HomeSwiper from './components/Swiper'
 export default {
   name: 'home',
-  components: { HomeHeader }
+  components: {HomeHeader, HomeSwiper}
 }
 </script>
---
+++ src/views/home/components/Swiper.vue
@@ -0,0 +1,43 @@
+<template>
+    <swiper class="swiper-wrapper" :options="swiperOption" ref="mySwiper">
+      <swiper-slide v-for="item in swiperSliders" :key="item.id">
+        <img class="swiper-img" :src="item.url">
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+</template>
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperSliders: [
+        {
+          id: '001',
+          url: 'https://source.qunarzz.com/site/images/wap/home/recommend/iphoneplus/dujia_shuqi_banner_20180710.jpg'
+        },
+        {
+          id: '002',
+          url: 'https://source.qunarzz.com/site/images/wap/home/recommend/iphoneplus/sc_sqdc_20180727.jpg'
+        }
+      ]
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+  .swiper-wrapper >>> .swiper-pagination-bullet-active
+    background-color #fff
+  .swiper-wrapper
+    width 100%
+    height 0
+    padding-bottom 24.5%
+    overflow hidden
+    background-color #eee
+  .swiper-img
+    width 100%
+</style>
